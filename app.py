1c0eda361ab0e883b8b1e9377a2946045edcb31a
HfdRama <hafiidhyrama@gmail.com>
2026-06-27 14:13:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,7 @@ init_chatbot_routes(app)
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all() 
-    app.run(debug=True)
+        db.create_all()
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
